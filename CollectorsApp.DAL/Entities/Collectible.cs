fbd6fd6cd26d513c960d8f0f6ef852f91220e2e4
bvermes <qerix8787@gmail.com>
2022-05-14 09:51:50 +0200

    update

--- CollectorsApp.DAL/Entities/Collectible.cs
+++ CollectorsApp.DAL/Entities/Collectible.cs
@@ -10,7 +10,6 @@ namespace CollectorsApp.DAL.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-
         public int BoughtFor { get; set; }
         public int Value { get; set; }
         public bool ForSale { get; set; }
